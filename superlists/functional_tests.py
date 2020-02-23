2b48a05d05f495cc09876f394057967fbc977378
ravi <ravilamkoti@gmail.com>
2020-02-23 19:32:23 +0530

    Front page HTML now generated from template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -32,7 +32,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy Peacocks feather' for row in rows)
+            any(row.text == '1: Buy Peacocks feather' for row in rows),
+            'New Item did not appeared'
         )
 
 
